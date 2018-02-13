6ee66570244450085dda89809cae8d73f1b13fd5
OXTS\clawford <clawford@oxts.com>
2018-02-13 16:28:06 +0000

    Inital Commit - Hello World

---
+++ ChrisLearningHelloWorld/ChrisLearningHelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChrisLearningHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Writing my first line of C# whilst actually learning the language
+            Console.WriteLine("Hello World!");
+
+            // Declaring my first variable and assigning it a value.
+            int score = 0;
+
+            // When you access a variable you are getting a copy
+            Console.WriteLine(score);
+
+
+
+        }
+    }
+}
